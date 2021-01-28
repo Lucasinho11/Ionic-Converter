59cf19c27072e710f6e93bb8d500b411706437ad
Lucas <lucas.lubasinski11@gmail.com>
2021-01-28 15:51:17 +0100

    add commponents and removed ts

---
+++ src/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+    <ion-router-outlet />
+    <ion-input type="number" value="333"></ion-input>
+    <form>
+
+    </form>
+</template>
+
+<script>
+
+import { IonRouterOutlet, IonInput } from '@ionic/vue';
+
+
+
+export default ({
+  name: 'Form',
+  data(){
+    return{
+      city: '',
+        
+    }
+  },
+  methods: {
+  },
+  components: {
+    IonRouterOutlet,
+    IonInput
+  }
+
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,26 +2,24 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>€uro Converter</ion-title>
       </ion-toolbar>
     </ion-header>
     
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-title size="large">€uro Converter</ion-title>
         </ion-toolbar>
       </ion-header>
-    
+      <Search/>
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
+<script>
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
